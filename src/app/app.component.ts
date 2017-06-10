ec8b00451ed6a8508ea4582e9ce4a104abc870c2
nckl <chris@nckl.ca>
2017-06-10 14:25:14 -0400

    Complete hero editor from tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+
 @Component({
   selector: 'App',
-  template:
-  `<div>
-    <h2>Welcome to {{name}} Angular2!</h2>
-  </div>`
+ template: `
+  <h1>{{title}}</h1>
+  <div>
+  <label>name: </label>
+  <input [(ngModel)]="hero.name" placeholder="name">
+</div>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div><label>name: </label>{{hero.name}}</div>
+  `
 })
 export class AppComponent implements OnInit {
-  public readonly name = 'electron-forge';
+ title= 'Tour of Heroes';
+ hero: Hero = {
+  id: 1,
+  name: 'Windstorm'
+};
 
   ngOnInit(): void {
     console.log('component initialized');
