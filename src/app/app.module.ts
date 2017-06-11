ff726b0419fc490be95504b419112d7df565ecf1
nckl <chris@nckl.ca>
2017-06-10 20:04:57 -0400

    Add mock web service
    Add mock web service and included update, add and delete methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './services/in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
@@ -12,7 +17,9 @@ import { AppRoutingModule }     from './app-routing.module';
 @NgModule({
   imports:      [ BrowserModule, 
                   FormsModule,
-                  AppRoutingModule ],
+                  AppRoutingModule,
+                  HttpModule,
+                  InMemoryWebApiModule.forRoot(InMemoryDataService), ],
   declarations: [ AppComponent,
   HeroDetailComponent,
   HeroesComponent,
